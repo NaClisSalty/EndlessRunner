df2c5fd28141a05f7914e15e53ebab162dc461ef
Ian Monahan <immonaha@ucsc.edu>
2020-04-22 18:42:55 -0700

    fix

---
+++ src/scenes/Play.js
@@ -0,0 +1,21 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+    init(data){}
+
+    preload() {} 
+
+    create() {
+        game.events.addListener(Phaser.Core.Events.FOCUS, this._onFocus, this);
+        game.events.addListener(Phaser.Core.Events.BLUR, this._onBlur, this);
+    }
+    update() {}
+
+    _onFocus() {
+        this.pauseGame(false);
+    }
+    _onBlur() {
+        this.pauseGame(true);
+    }
+}
