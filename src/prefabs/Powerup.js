8e654aa706de2aba67d7c613d8e2914d3013885d
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-25 04:03:45 -0700

    just planing and notes

--- src/prefabs/Powerup.js
+++ src/prefabs/Powerup.js
@@ -3,4 +3,20 @@ class Powerup extends Phaser.GameObjects.Sprite{
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
     }
+
+    update(){
+        //speed for upgrades
+        this.x -= game.settings.powerSpeed;
+    }
+
+    checkPower(){
+        //function to check which powerup it is based off of the image file assigned to it
+        //may need to be in a diff .js file
+    }
+
+    randomizePower(){
+        //function to randomize powerups as the are created (may need to be in a differnt .js file)
+    }
+
+
 }
