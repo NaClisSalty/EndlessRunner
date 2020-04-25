107de97b44d4b762361e001fb79f2cb27d7bbf91
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-25 15:47:08 -0700

    powerup design ideas

--- src/prefabs/Powerup.js
+++ src/prefabs/Powerup.js
@@ -4,6 +4,22 @@ class Powerup extends Phaser.GameObjects.Sprite{
         scene.add.existing(this);
     }
 
+    /*
+    Powerups we want:
+    1) Make the player bigger
+    2) Make the player smaller
+    3) Give the player a shield
+    4) Increase player speed
+    5) Decrease player speed
+    6) set player equal to mouse
+    7) player automatically shoots objects to hit and destroy obstacles
+    8) Mabey one special character that picks one of the above poerups but the effect is permanent
+    9) Increase obstacle and powerup speed
+    10) Cursor/ Player will now temporarily destroy obstacles on contact (star invincibility from mario)
+
+    */
+
+
     update(){
         //speed for upgrades
         this.x -= game.settings.powerSpeed;
