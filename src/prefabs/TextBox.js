9368a63b516302443cd1ff2f5b7d9faf811df1d2
Darcy Phipps <diphipps@ucsc.edu>
2020-05-03 22:36:00 -0700

    Refactored TextBox

--- src/prefabs/TextBox.js
+++ src/prefabs/TextBox.js
@@ -2,11 +2,14 @@
 
 class TextBox extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture, frame, pointValue) {
+        //set up basic properties
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.points = pointValue;
         scene.physics.add.existing(this);
+        //Make sure it doesn't bounce off things
         this.setImmovable(true);
+        //Store the score
+        this.points = pointValue;
         //speed
         this.setVelocityX(-125 - (scene.time.now * 0.005));
         //Check to see if it's made the next barrier
